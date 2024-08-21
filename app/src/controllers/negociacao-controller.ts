74313ebc7029051f0ba26295d1fd072b16dced4a
divino-souza <divino.souza@tecnospeed.com.br>
2024-08-21 17:05:05 -0300

    Comitando aulas type script

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { logarTempoExecucao } from '../decorators/logar-tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
@@ -9,7 +10,7 @@ export class NegociacaoController {
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
-    private negociacoesView = new NegociacoesView('#negociacoesView', true);
+    private negociacoesView = new NegociacoesView('#negociacoesView');
     private mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
@@ -18,8 +19,10 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor') as HTMLInputElement;
         this.negociacoesView.update(this.negociacoes);
     }
-
-    public adiciona(): void {
+     
+     @logarTempoExecucao()
+     public adiciona(): void {
+               
         /*
             Zé, você já viu isso?
         */
@@ -38,6 +41,7 @@ export class NegociacaoController {
         this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
         this.atualizaView();
+        
     }
 
     private ehDiaUtil(data: Date) {
--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,27 +1,29 @@
+import { inspect } from "../decorators/inspect.js";
+import { logarTempoExecucao } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
-    private escapar = false;
 
-    constructor(seletor: string, escapar?: boolean) {
+    constructor(seletor: string) {
         const elemento = document.querySelector(seletor);
         if (elemento) {
             this.elemento = elemento as HTMLElement;
         } else {
             throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
         }
-        if (escapar) {
-            this.escapar = escapar;
-        }
+    
     }
-
+    @inspect
+    @logarTempoExecucao(true)
     public update(model: T): void {
+
+        const t1 = performance.now();
         let template = this.template(model);
-        if (this.escapar) {
-            template = template
-                .replace(/<script>[\s\S]*?<\/script>/, '');
-        }
+      
         this.elemento.innerHTML = template;
+        
+      
     }
 
     protected abstract template(model: T): string;
